6e131449b1a9a446c9e1a187747d59d601a915b2
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-09-17 15:05:28 -0400

    Unit Tests passed

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,32 @@
 const {shuffleArray} = require('./utils')
 
+let testData = [
+    {
+        id: 0,
+        name: "Juan",
+        health: 100
+    },
+
+    {
+        id: 1,
+        name: "Fede",
+        health: 50
+    },
+
+    {
+        id:2,
+        name: "Derek",
+        health: 75
+    }
+]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
-})
+    test("return length of array", () => {
+        const shuffledArray = shuffleArray(testData)
+        expect(shuffledArray.length).toBe(testData.length)
+    });
+
+    test("can assess testData", () => {
+        expect(testData[1].name).toBe("Fede")
+    });
+});
